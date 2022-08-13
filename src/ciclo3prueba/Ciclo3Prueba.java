c4dad02eff0e1d049c165f1e443f4a5a5287a7a6
unknown <jefferson02830@gmail.comm>
2022-08-13 16:06:08 -0500

    primer comit

---
+++ src/ciclo3prueba/Ciclo3Prueba.java
@@ -0,0 +1,8 @@
+package ciclo3prueba;
+public class Ciclo3Prueba {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Work");
+    }
+    
+}
